a697ed99594ed2fd5118e3a918d5330d8962b4ee
Ekyeu <115065962+Ekyeu@users.noreply.github.com>
2024-02-21 10:19:00 +0100

    Add files via upload

---
+++ sortowanieBabelkowe/sortowanieBabelkowe/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine("Podaj dlugosc tablicy");
+int dlugosc = Convert.ToInt32(Console.ReadLine());
+
+//sortowanie babelkowe
+
+int[] array = new int[dlugosc];
+Random random = new Random();
+
+
+for (int i = 0; i < dlugosc; i++)
+{
+    array[i] = random.Next(1, 100);
+    Console.Write(array[i]+" ");
+}
+
+
+int temp;
+
+for (int i = 0; i < dlugosc; i++)
+{
+    for (int j = 1; j < dlugosc - i; j++)
+    {
+        if (array[j - 1] > array[j])
+        {
+            temp = array[j - 1];
+            array[j - 1] = array[j];
+            array[j] = temp;
+        }
+    }
+}
+
+Console.WriteLine("\nPosortowana tablica");
+for (int i = 0; i < dlugosc; i++)
+{
+    Console.Write(array[i] + " ");
+}
+
+//sortowanie kubelkowe
